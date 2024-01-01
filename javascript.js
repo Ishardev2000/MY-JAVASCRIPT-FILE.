02ec24cdea9f6d9a5bb157a5884882b64126c964
ISHAR KHAN 601 <145116966+Ishardev2000@users.noreply.github.com>
2024-01-01 11:04:56 +0500

    WEBSITE HTML CSS AND JS

--- javascript.js
+++ javascript.js
@@ -1,59 +1,69 @@
-const HOUR_DEGREES_PER_SECOND = 360 / (12 * 60 * 60);
-const MINUTE_DEGREES_PER_SECOND = 360 / (60 * 60);
-const SECOND_DEGREES_PER_SECOND = 360 / 60;
 
-function startclock(clock) {
-  const hourHand = clock.querySelector(".hour-hand");
-  const minuteHand = clock.querySelector(".minute-hand");
-  const secondHand = clock.querySelector(".second-hand");
+$(document).ready(function () {
+  $('.fa-bars').click(function () {
+      $('.nav-links').slideToggle(3000);
+  })
+})
 
-  const now = new Date();
-  const hours = now.getHours() % 12;
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
+$(window).scroll(function () {
+  let scroll = $(window).scrollTop();
+  let width = $(window).width()
 
-  const fractionalMinutes = minutes + seconds / 60;
-  const fractionalHours = hours + fractionalMinutes / 60;
 
-  hourHand.style.setProperty(
-    "--_rotation",
-    (360 * fractionalHours) / 12 + "deg"
-  );
 
-  minuteHand.style.setProperty(
-    "--_rotation",
-    (360 * fractionalMinutes) / 60 + "deg"
-  );
+  if (scroll >= 70 && width >= 995) {
+      $('nav').addClass("new-nav");
 
-  secondHand.style.setProperty("--_rotation", (360 * seconds) / 60 + "deg");
+      $('nav ul li a').css({"color":"black"})
 
-  setInterval(() => {
-    const hoursRotation = parseFloat(
-      hourHand.style.getPropertyValue("--_rotation")
-    );
-    hourHand.style.setProperty(
-      "--_rotation",
-      hoursRotation + HOUR_DEGREES_PER_SECOND + "deg"
-    );
+      $('.nav-heading span').css({"color":"black"})
+      $('.nav-heading span>i').css({"color":"#F85A40"})
+      $('.fa-moon').css({"color":"black"})
 
-    const minutesRotation = parseFloat(
-      minuteHand.style.getPropertyValue("--_rotation")
-    );
-    minuteHand.style.setProperty(
-      "--_rotation",
-      minutesRotation + MINUTE_DEGREES_PER_SECOND + "deg"
-    );
 
-    const secondsRotation = parseFloat(
-      secondHand.style.getPropertyValue("--_rotation")
-    );
-    secondHand.style.setProperty(
-      "--_rotation",
-      secondsRotation + SECOND_DEGREES_PER_SECOND + "deg"
-    );
-  }, 1000);
-}
 
-window.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".clock").forEach((clock) => startclock(clock));
-});
+       $('nav ul li a').css({"color": "black"})
+
+       $('.nav-heading span').css({"color":"black"})
+       $('.nav-heading span>i').css({"color":"#F85A40"})
+
+     
+     
+   
+  }
+
+  else if (scroll==0 && width >= 995){
+      $('nav').removeClass("new-nav");
+      $('nav ul li a').css({"color":"#fff"})
+      $('.nav-heading span>i').css({"color":"#fff"})
+     
+
+      $('.nav-heading span').css({"color":"white"})
+
+      $('.fa-moon').css({"color":"#fff"})
+      // $('nav ul li a').css({"color": "#fff"})
+
+      // $('.nav-heading span').css({"color":"#fff"})
+      // $('.nav-heading span>i').css({"color":"#fff"})
+
+  }
+  else if(scroll>=70 && width<995){
+      $('.nav-heading span').css({"color":"black"})
+      $('nav').addClass("new-nav");
+      $('.nav-heading span').addClass("black")
+      $('.nav-heading span>i').css({"color":"#F85A40"})
+      $('.fa-moon').css({"color":"black"})
+
+  }
+
+  else if(scroll ==0 && width < 995 ){
+      $('nav').removeClass("new-nav");
+      $('.nav-heading span').css({"color":"white"})
+      $('.nav-heading span').removeClass("black")
+      $('.fa-moon').css({"color":"white"})
+
+      $('.nav-links>span>i').css({"color":"#fff"})
+
+  }
+})
+
