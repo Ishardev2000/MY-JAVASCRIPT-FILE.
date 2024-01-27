b70ab52a5d1ea5d79535cb99687357b99d541369
ISHAR KHAN 601 <145116966+Ishardev2000@users.noreply.github.com>
2024-01-27 19:21:19 +0500

    RV IN  OPRATES JS
    RV AND ALL OPRATES IN JAVASCRIPT JAVASCRIPT.

---
+++ 1 javascript.js
@@ -0,0 +1,10 @@
+//non primituive data typs
+//object
+const school= {
+    fullname: "gornament school",
+    name : "school",
+    number : 121,
+    pakistan: true,
+};
+document.write(school);
+console.log("school");
